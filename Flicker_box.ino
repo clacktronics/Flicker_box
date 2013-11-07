922b23b96ba398e3e1cae3f3f8dabacbef2da5c3
clacktronics <clacktronics@gmail.com>
2013-11-07 22:25:08 +0000

    Initial example of the flicker program

---
+++ Flicker_box.ino
@@ -0,0 +1,59 @@
+/*
+***********************************
+Bulb flicker program by Ben Barwise
+***********************************
+Version (alpha) 0.1  - 07/11/2013
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+This code does not work it simply shows the basic structure
+of the pattern but flickers unrealistically and predictably.
+
+About:
+I wrote this program to learn about git (and github)
+so I can develop my programming skills and develop
+my understanding on how to release code, code versioning
+and licencing.
+
+This program is used to control an LED strip on and off 
+in patterns that simulate the faliure of a flourecent lamp.
+Similar code I created was used to make a lighbox flicker 
+for a piece of work by Ryan Gander.
+
+I reccomend using a MOSFET like IRF540 to drive the strip.
+
+Typically the pattern seems to be a burst of flickers ended
+by a held period before it fails.
+
+There are variables that can be changed to simulate different 
+feels.
+
+such as 
+* Try to start, breif on period then off and restart
+* Constant fast random flickering
+* on with Intermittent off flickers
+* Long periods of off with occasional signs of life
+
+********************************************************
+
+*/
+
+
+
+void setup() {
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+
+// Startup flashes
+for (int i; i < 100; i++){
+  digitalWrite(13,0);
+  delay(10);
+  digitalWrite(13,1);
+  delay(10);
+}
+
+delay(10); // Loop ends with on so this delay then decides sucess hold
+digitalWrite(13,0); // This is then the faliure
+delay(10); // how long to wait before next startup attempt
+
+
